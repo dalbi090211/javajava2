ca0ac568a4cb619e1363576d25ed725c34b17167
dalbi090211 <trump0911@naver.com>
2022-10-19 03:29:16 +0000

    [sdf]

--- Eighth.java
+++ Eighth.java
@@ -1,12 +1,29 @@
 public class Eighth {
     public static int i = 0;
+    public static int j = 0;
     public static java.util.Scanner sc  = new java.util.Scanner(System.in);
     public static void main(String args[]){
-        System.out.println("{ ** 2단 **");
-        System.out.println("2 * 1 = 2");
-        for(i = 3; i < 9; i+=2){
-            System.out.println("2 * " + i + " = " + i*2);
+        int end = 0;
+        int max = 0;
+        System.out.print("목표 값 : ");
+        end = sc.nextInt();
+        System.out.print("소수 : ");
+        for(i = 1; i < end; i++){
+            if(i < 10){
+                max = i;
+            }
+            else{
+                max = i /2;
+            }
+            for(j = 2 ; j < max; j++){
+                if(i%j== 0){
+                    j = i+1;
+                }
+            }
+            if(j < i + 1){
+                System.out.print(i + " ");
+            }
         }
-        System.out.println("2 * 9 = 18 }");
+        System.out.println();
     }
 }
